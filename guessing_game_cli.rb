14a3db84d8897bdbf7206c22d8113200c8867878
Marcia Freeman <marciafreeman2011@gmail.com>
2020-10-24 21:55:40 +0000

    Done.

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,1 +1,17 @@
-# Code your solution here!
+
+def run_guessing_game
+ puts "Choose a number between 1-6, or type 'exit' to escape"
+  player_input = gets.chomp
+  comp_num = (rand(6)+1).to_s
+ puts player_input
+ puts comp_num
+
+  if player_input == "exit"
+    puts "Goodbye!"
+  elsif player_input === comp_num
+    puts "You guessed the correct number!"
+  else
+    puts "Sorry! The computer guessed #{comp_num}."
+  end
+
+end
